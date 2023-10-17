a7ada165c68ede36c68af2774c5280132a629acd
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-10-17 20:12:39 +0600

    Environment setup

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Error 404</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Welcome Home</h2>
+        </div>
+    );
+};
+
+export default Home;
